3e5b5236227ac07dc52dcc44587c086183a301ac
Jane <JaneyBooboo@users.noreply.github.com>
2016-09-26 14:09:57 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Assignment: to cache the invere of a square matrix
 
-## Write a short comment describing this function
+
+# creates a special square “matrix” object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  InvertMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    InvertMatrix <<- NULL
+  }
+  get <- function() x
+  setInvertedMatrix <- function(inverse) InvertMatrix <<- inverse
+  getInvertedMatrix <- function() InvertMatrix
+  list(set = set, get = get, setInvertedMatrix = setInvertedMatrix, getInvertedMatrix = getInvertedMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+# returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  InvertMatrix <- x$getInvertedMatrix()
+  if(!is.null(InvertMatrix)) {
+    message("getting cached data")
+    return(InvertMatrix)
+  }
+  matrixdata <- x$getInvertedMatrix()
+  InvertMatrix <- solve(matrixdata, ...)  # solves for inverse of square matrix
+  x$setInvertedMatrix(InvertedMatrix)
+  return(InvertMatrix)  # Return a matrix that is the inverse of 'x'
 }
+        
+
+
+
